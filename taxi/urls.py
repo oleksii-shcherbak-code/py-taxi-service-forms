96dcaa3f867001d9c5a5773ab72db18d77a6f4ed
Oleksii Shcherbak <qksardas001@mail.com>
2025-12-19 20:39:35 +0200

    fix bugs

--- taxi/urls.py
+++ taxi/urls.py
@@ -33,15 +33,15 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("manufacturer/create/",
+    path("manufacturers/create/",
          ManufacturerCreateView.as_view(),
          name="manufacturer-create"
          ),
-    path("manufacturer/<int:pk>/update/",
+    path("manufacturers/<int:pk>/update/",
          ManufacturerUpdateView.as_view(),
          name="manufacturer-update"
          ),
-    path("manufacturer/<int:pk>/delete/",
+    path("manufacturers/<int:pk>/delete/",
          ManufacturerDeleteView.as_view(),
          name="manufacturer-delete"
          ),
